9c75eed15f0ce3bc849d6b9ddf5ba07a1cc4f12a
rg687076 <tokushi2011@gmail.com>
2021-08-12 17:38:55 +0900

    中華フォント対応 いろいろ、文言追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,11 +9,21 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    const locale = Locale("ja", "JP");
     return MaterialApp(
       title: '安買高売',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      locale: locale,
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        locale,
+      ],
       home: HomePage(title: '仮想通貨の現在売買値'),
     );
   }
@@ -40,37 +51,44 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
+      body: DefaultTextStyle.merge(
+        style: TextStyle(fontSize: 24,),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Table(
-              border: TableBorder.all(),
-              children: [TableRow(children: [Padding(padding: EdgeInsets.all(2.0),child: Text(""))        ,Padding(padding: EdgeInsets.all(2.0),child: Text("着金")),Padding(padding: EdgeInsets.all(2.0),child: Text("送金[USDT]"))         ,Padding(padding: EdgeInsets.all(2.0),child: Text("送金[BTC]"))   ,Padding(padding: EdgeInsets.all(2.0),child: Text("送金[ETH]"))   ,Padding(padding: EdgeInsets.all(2.0),child: Text("送金[XRP]")),Padding(padding: EdgeInsets.all(2.0),child: Text("送金[BNB]"))    ,Padding(padding: EdgeInsets.all(2.0),child: Text("買い[%]")),Padding(padding: EdgeInsets.all(2.0),child: Text("売り[%]")),]),
-                         TableRow(children: [Padding(padding: EdgeInsets.all(2.0),child: Text("Binance")) ,Padding(padding: EdgeInsets.all(2.0),child: Text("0"))   ,Padding(padding: EdgeInsets.all(2.0),child: Text("1(TRC20)\n15(ERC20)")),Padding(padding: EdgeInsets.all(2.0),child: Text("0.00057(BTC)")),Padding(padding: EdgeInsets.all(2.0),child: Text("0.003(BEP20)")),Padding(padding: EdgeInsets.all(2.0),child: Text("0.25(XRP)")),Padding(padding: EdgeInsets.all(2.0),child: Text("0.0005(BEP20)")),Padding(padding: EdgeInsets.all(2.0),child: Text("0.1"))    ,Padding(padding: EdgeInsets.all(2.0),child: Text("0.1"))    ,]),
-                         TableRow(children: [Padding(padding: EdgeInsets.all(2.0),child: Text("FTX"))     ,Padding(padding: EdgeInsets.all(2.0),child: Text("0"))   ,Padding(padding: EdgeInsets.all(2.0),child: Text("0"))                  ,Padding(padding: EdgeInsets.all(2.0),child: Text("0"))           ,Padding(padding: EdgeInsets.all(2.0),child: Text("0"))           ,Padding(padding: EdgeInsets.all(2.0),child: Text("0"))        ,Padding(padding: EdgeInsets.all(2.0),child: Text("0"))            ,Padding(padding: EdgeInsets.all(2.0),child: Text("0.07"))   ,Padding(padding: EdgeInsets.all(2.0),child: Text("0.07"))   ,]),
-                         TableRow(children: [Padding(padding: EdgeInsets.all(2.0),child: Text("KuCoin"))  ,Padding(padding: EdgeInsets.all(2.0),child: Text("0"))   ,Padding(padding: EdgeInsets.all(2.0),child: Text("1(TRC20)\n20(ERC20)")),Padding(padding: EdgeInsets.all(2.0),child: Text("0.0006(BTC)")) ,Padding(padding: EdgeInsets.all(2.0),child: Text("0.004(ERC20)")),Padding(padding: EdgeInsets.all(2.0),child: Text("0.2(XRP)")) ,Padding(padding: EdgeInsets.all(2.0),child: Text("0.02(BEP20)"))  ,Padding(padding: EdgeInsets.all(2.0),child: Text("0.1"))    ,Padding(padding: EdgeInsets.all(2.0),child: Text("0.1"))    ,]),
-                         TableRow(children: [Padding(padding: EdgeInsets.all(2.0),child: Text("Bitstamp")),Padding(padding: EdgeInsets.all(2.0),child: Text("0"))   ,Padding(padding: EdgeInsets.all(2.0),child: Text("	2.5"))               ,Padding(padding: EdgeInsets.all(2.0),child: Text("0.0005"))      ,Padding(padding: EdgeInsets.all(2.0),child: Text("0.0035"))      ,Padding(padding: EdgeInsets.all(2.0),child: Text("0.02"))     ,Padding(padding: EdgeInsets.all(2.0),child: Text("---"))          ,Padding(padding: EdgeInsets.all(2.0),child: Text("0.5"))    ,Padding(padding: EdgeInsets.all(2.0),child: Text("0.5"))    ,]),
-                         TableRow(children: [Padding(padding: EdgeInsets.all(2.0),child: Text("Poloniex")),Padding(padding: EdgeInsets.all(2.0),child: Text("0"))   ,Padding(padding: EdgeInsets.all(2.0),child: Text("0"))                  ,Padding(padding: EdgeInsets.all(2.0),child: Text("0"))           ,Padding(padding: EdgeInsets.all(2.0),child: Text("0"))           ,Padding(padding: EdgeInsets.all(2.0),child: Text("0"))        ,Padding(padding: EdgeInsets.all(2.0),child: Text("0"))            ,Padding(padding: EdgeInsets.all(2.0),child: Text("0.155"))  ,Padding(padding: EdgeInsets.all(2.0),child: Text("0.155"))  ,]),
-                         TableRow(children: [Padding(padding: EdgeInsets.all(2.0),child: Text("Bittrex")) ,Padding(padding: EdgeInsets.all(2.0),child: Text("0"))   ,Padding(padding: EdgeInsets.all(2.0),child: Text("69"))                 ,Padding(padding: EdgeInsets.all(2.0),child: Text("0.0003"))      ,Padding(padding: EdgeInsets.all(2.0),child: Text("0.0137"))      ,Padding(padding: EdgeInsets.all(2.0),child: Text("1"))        ,Padding(padding: EdgeInsets.all(2.0),child: Text("---"))          ,Padding(padding: EdgeInsets.all(2.0),child: Text("0.35"))   ,Padding(padding: EdgeInsets.all(2.0),child: Text("0.35"))   ,]),
-                         TableRow(children: [Padding(padding: EdgeInsets.all(2.0),child: Text("OKEx"))    ,Padding(padding: EdgeInsets.all(2.0),child: Text("0"))   ,Padding(padding: EdgeInsets.all(2.0),child: Text("0.88(ERC20)"))        ,Padding(padding: EdgeInsets.all(2.0),child: Text("0.0004"))      ,Padding(padding: EdgeInsets.all(2.0),child: Text("0.003"))       ,Padding(padding: EdgeInsets.all(2.0),child: Text("0.1"))      ,Padding(padding: EdgeInsets.all(2.0),child: Text("---"))          ,Padding(padding: EdgeInsets.all(2.0),child: Text("0.1"))    ,Padding(padding: EdgeInsets.all(2.0),child: Text("0.1"))    ,]),
-                         TableRow(children: [Padding(padding: EdgeInsets.all(2.0),child: Text("KuCoin"))  ,Padding(padding: EdgeInsets.all(2.0),child: Text("0"))   ,Padding(padding: EdgeInsets.all(2.0),child: Text("0(TRC20)\n5(ERC20)")) ,Padding(padding: EdgeInsets.all(2.0),child: Text("0.0007"))      ,Padding(padding: EdgeInsets.all(2.0),child: Text("0.01"))        ,Padding(padding: EdgeInsets.all(2.0),child: Text("0.25"))     ,Padding(padding: EdgeInsets.all(2.0),child: Text("---"))          ,Padding(padding: EdgeInsets.all(2.0),child: Text("0.1"))    ,Padding(padding: EdgeInsets.all(2.0),child: Text("0.1"))    ,]),
-                        ],),
-          ],
-        ),
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children:[Text("下記の現在地を表示しています。"),
+                      Text("　・通貨ペア(BTC/USDT,ETH/USDT,XRP/USDT,BNB/USDT)の現在値"),
+                      Text("　・現在で売買した時の利益金額"),
+                      Text("　※基軸通貨には、USDTを選んでいます。(JPY(日本円)がわかりやすいですが、業者が国内に限られてしまうので。)"),
+                      Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text('You have pushed the button this many times:',),
+                            Text('$_counter', style: Theme.of(context).textTheme.headline4, ),
+                            Table(
+                              border: TableBorder.all(),
+                              children: [TableRow(children: [Padding(padding: EdgeInsets.all(4.0),child: Text(""))        ,Padding(padding: EdgeInsets.all(4.0),child: Text("着金")),Padding(padding: EdgeInsets.all(4.0),child: Text("送金[USDT]"))         ,Padding(padding: EdgeInsets.all(4.0),child: Text("送金[BTC]"))   ,Padding(padding: EdgeInsets.all(4.0),child: Text("送金[ETH]"))   ,Padding(padding: EdgeInsets.all(4.0),child: Text("送金[XRP]")),Padding(padding: EdgeInsets.all(4.0),child: Text("送金[BNB]"))    ,Padding(padding: EdgeInsets.all(4.0),child: Text("買い[%]")),Padding(padding: EdgeInsets.all(4.0),child: Text("売り[%]")),]),
+                                         TableRow(children: [Padding(padding: EdgeInsets.all(4.0),child: Text("Binance")) ,Padding(padding: EdgeInsets.all(4.0),child: Text("0"))   ,Padding(padding: EdgeInsets.all(4.0),child: Text("1(TRC20)\n15(ERC20)")),Padding(padding: EdgeInsets.all(4.0),child: Text("0.00057(BTC)")),Padding(padding: EdgeInsets.all(4.0),child: Text("0.003(BEP20)")),Padding(padding: EdgeInsets.all(4.0),child: Text("0.25(XRP)")),Padding(padding: EdgeInsets.all(4.0),child: Text("0.0005(BEP20)")),Padding(padding: EdgeInsets.all(4.0),child: Text("0.1"))    ,Padding(padding: EdgeInsets.all(4.0),child: Text("0.1"))    ,]),
+                                         TableRow(children: [Padding(padding: EdgeInsets.all(4.0),child: Text("FTX"))     ,Padding(padding: EdgeInsets.all(4.0),child: Text("0"))   ,Padding(padding: EdgeInsets.all(4.0),child: Text("0"))                  ,Padding(padding: EdgeInsets.all(4.0),child: Text("0"))           ,Padding(padding: EdgeInsets.all(4.0),child: Text("0"))           ,Padding(padding: EdgeInsets.all(4.0),child: Text("0"))        ,Padding(padding: EdgeInsets.all(4.0),child: Text("0"))            ,Padding(padding: EdgeInsets.all(4.0),child: Text("0.07"))   ,Padding(padding: EdgeInsets.all(4.0),child: Text("0.07"))   ,]),
+                                         TableRow(children: [Padding(padding: EdgeInsets.all(4.0),child: Text("KuCoin"))  ,Padding(padding: EdgeInsets.all(4.0),child: Text("0"))   ,Padding(padding: EdgeInsets.all(4.0),child: Text("1(TRC20)\n20(ERC20)")),Padding(padding: EdgeInsets.all(4.0),child: Text("0.0006(BTC)")) ,Padding(padding: EdgeInsets.all(4.0),child: Text("0.004(ERC20)")),Padding(padding: EdgeInsets.all(4.0),child: Text("0.2(XRP)")) ,Padding(padding: EdgeInsets.all(4.0),child: Text("0.02(BEP20)"))  ,Padding(padding: EdgeInsets.all(4.0),child: Text("0.1"))    ,Padding(padding: EdgeInsets.all(4.0),child: Text("0.1"))    ,]),
+                                         TableRow(children: [Padding(padding: EdgeInsets.all(4.0),child: Text("Bitstamp")),Padding(padding: EdgeInsets.all(4.0),child: Text("0"))   ,Padding(padding: EdgeInsets.all(4.0),child: Text("	2.5"))               ,Padding(padding: EdgeInsets.all(4.0),child: Text("0.0005"))      ,Padding(padding: EdgeInsets.all(4.0),child: Text("0.0035"))      ,Padding(padding: EdgeInsets.all(4.0),child: Text("0.02"))     ,Padding(padding: EdgeInsets.all(4.0),child: Text("---"))          ,Padding(padding: EdgeInsets.all(4.0),child: Text("0.5"))    ,Padding(padding: EdgeInsets.all(4.0),child: Text("0.5"))    ,]),
+                                         TableRow(children: [Padding(padding: EdgeInsets.all(4.0),child: Text("Poloniex")),Padding(padding: EdgeInsets.all(4.0),child: Text("0"))   ,Padding(padding: EdgeInsets.all(4.0),child: Text("0"))                  ,Padding(padding: EdgeInsets.all(4.0),child: Text("0"))           ,Padding(padding: EdgeInsets.all(4.0),child: Text("0"))           ,Padding(padding: EdgeInsets.all(4.0),child: Text("0"))        ,Padding(padding: EdgeInsets.all(4.0),child: Text("0"))            ,Padding(padding: EdgeInsets.all(4.0),child: Text("0.155"))  ,Padding(padding: EdgeInsets.all(4.0),child: Text("0.155"))  ,]),
+                                         TableRow(children: [Padding(padding: EdgeInsets.all(4.0),child: Text("Bittrex")) ,Padding(padding: EdgeInsets.all(4.0),child: Text("0"))   ,Padding(padding: EdgeInsets.all(4.0),child: Text("69"))                 ,Padding(padding: EdgeInsets.all(4.0),child: Text("0.0003"))      ,Padding(padding: EdgeInsets.all(4.0),child: Text("0.0137"))      ,Padding(padding: EdgeInsets.all(4.0),child: Text("1"))        ,Padding(padding: EdgeInsets.all(4.0),child: Text("---"))          ,Padding(padding: EdgeInsets.all(4.0),child: Text("0.35"))   ,Padding(padding: EdgeInsets.all(4.0),child: Text("0.35"))   ,]),
+                                         TableRow(children: [Padding(padding: EdgeInsets.all(4.0),child: Text("OKEx"))    ,Padding(padding: EdgeInsets.all(4.0),child: Text("0"))   ,Padding(padding: EdgeInsets.all(4.0),child: Text("0.88(ERC20)"))        ,Padding(padding: EdgeInsets.all(4.0),child: Text("0.0004"))      ,Padding(padding: EdgeInsets.all(4.0),child: Text("0.003"))       ,Padding(padding: EdgeInsets.all(4.0),child: Text("0.1"))      ,Padding(padding: EdgeInsets.all(4.0),child: Text("---"))          ,Padding(padding: EdgeInsets.all(4.0),child: Text("0.1"))    ,Padding(padding: EdgeInsets.all(4.0),child: Text("0.1"))    ,]),
+                                         TableRow(children: [Padding(padding: EdgeInsets.all(4.0),child: Text("KuCoin"))  ,Padding(padding: EdgeInsets.all(4.0),child: Text("0"))   ,Padding(padding: EdgeInsets.all(4.0),child: Text("0(TRC20)\n5(ERC20)")) ,Padding(padding: EdgeInsets.all(4.0),child: Text("0.0007"))      ,Padding(padding: EdgeInsets.all(4.0),child: Text("0.01"))        ,Padding(padding: EdgeInsets.all(4.0),child: Text("0.25"))     ,Padding(padding: EdgeInsets.all(4.0),child: Text("---"))          ,Padding(padding: EdgeInsets.all(4.0),child: Text("0.1"))    ,Padding(padding: EdgeInsets.all(4.0),child: Text("0.1"))    ,]),
+                              ],),
+                            /* \u{2252}...≒ */
+                            Text('※0.0005[BTC]\u{2252}2,000(円)　※0.003[ETH]\u{2252}1,030(円)　※0.2[XRP]\u{2252}18(円)　※0.02[BNB]\u{2252}780(円)',),
+                          ],
+                        ),
+                      ),]
+          ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
