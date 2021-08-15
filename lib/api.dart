9bb15a5baf3f7867ba3edf4dc62467842f5d32f8
rg687076 <tokushi2011@gmail.com>
2021-08-15 21:01:38 +0900

    KuCoinからAsk/Bidを取得する処理を作成。

--- lib/api.dart
+++ lib/api.dart
@@ -14,7 +14,7 @@ Future<Map<Symbol, AskBidDataforTbl>> GetTicker(BrokerId bid) async {
   switch (bid) {
     case BrokerId.bi: retjson = getTickerBi(pairs); break;
     case BrokerId.fx: retjson = getTickerFx(pairs); break;
-    case BrokerId.kc: retjson = getTickerBi(pairs); break;
+    case BrokerId.kc: retjson = getTickerKc(pairs); break;
     case BrokerId.bs: retjson = getTickerBi(pairs); break;
     case BrokerId.pn: retjson = getTickerBi(pairs); break;
     case BrokerId.bt: retjson = getTickerBi(pairs); break;
@@ -84,6 +84,42 @@ Future<Map<Symbol, AskBidDataforTbl>> getTickerFx(Set<Symbol> pairs) async {
     if(item.containsKey('name') && item.containsKey('bid') && item.containsKey('ask')) {
       Symbol sym = str2symbol(item['name']);
       ret[sym] = AskBidDataforTbl(sym, BrokerId.fx, item['ask'].toString(), item['bid'].toString());
+    }
+  }
+
+  return ret;
+}
+
+/* KuCoinからAsk/Bid取得 */
+Future<Map<Symbol, AskBidDataforTbl>> getTickerKc(Set<Symbol> pairs) async {
+  final Set<String> PairStrs =  pairs.map((s)=>s.str.replaceFirst("_", "-")).toSet();
+
+  final String endpoint = 'https://api.kucoin.com';
+  final String method = '/api/v1/market/allTickers';
+
+  final http.Response retRes = await http.get(Uri.parse(endpoint+method), headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "POST, GET, OPTIONS, PUT, DELETE, HEAD",
+  });
+  print('--------------');
+  final retjson2 = await json.decode(retRes.body)['data']['ticker'].where((item)=>PairStrs.contains(item['symbol'])).toList();
+  print(retjson2);
+
+  Symbol Function(String) str2symbol = (String sybl) {
+    switch(sybl) {
+      case 'BTC-USDT': return Symbol.BTC_USDT;
+      case 'ETH-USDT': return Symbol.ETH_USDT;
+      case 'XRP-USDT': return Symbol.XRP_USDT;
+      case 'BNB-USDT': return Symbol.BNB_USDT;
+      default: throw Exception("例外発生!! 未サポートペア =" + sybl);
+    }
+  };
+
+  Map<Symbol, AskBidDataforTbl> ret = {};
+  for(final item in retjson2) {
+    if(item.containsKey('symbol') && item.containsKey('buy') && item.containsKey('sell')) {
+      Symbol sym = str2symbol(item['symbol']);
+      ret[sym] = AskBidDataforTbl(sym, BrokerId.kc, item['sell'].toString(), item['buy'].toString());
     }
   }
 
