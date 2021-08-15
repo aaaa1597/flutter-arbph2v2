fc503dfd5c94f24d95fc04c82fd3c57ac149058c
rg687076 <tokushi2011@gmail.com>
2021-08-15 20:30:03 +0900

    FTXからAsk/Bidを取得する処理を作成完了。 CPRS(Cross-Origin Resource Sharing クロスオリジンリソース共有)が原因で、取得失敗してたけど、それも解決。 ※実際には、下記の対応も必要。 1- Go to flutter\bin\cache and remove a file named: flutter_tools.stamp 2- Go to flutter\packages\flutter_tools\lib\src\web and open the file chrome.dart. 3- Find '--disable-extensions' 4- Add '--disable-web-security' 参考 : https://stackoverflow.com/questions/65630743/how-to-solve-flutter-web-api-cors-error-only-with-dart-code

--- lib/api.dart
+++ lib/api.dart
@@ -13,7 +13,7 @@ Future<Map<Symbol, AskBidDataforTbl>> GetTicker(BrokerId bid) async {
   Future<Map<Symbol, AskBidDataforTbl>> retjson;
   switch (bid) {
     case BrokerId.bi: retjson = getTickerBi(pairs); break;
-    case BrokerId.fx: retjson = getTickerBi(pairs); break;
+    case BrokerId.fx: retjson = getTickerFx(pairs); break;
     case BrokerId.kc: retjson = getTickerBi(pairs); break;
     case BrokerId.bs: retjson = getTickerBi(pairs); break;
     case BrokerId.pn: retjson = getTickerBi(pairs); break;
@@ -36,20 +36,54 @@ Future<Map<Symbol, AskBidDataforTbl>> getTickerBi(Set<Symbol> pairs) async {
   final retjson2 = await json.decode(retRes.body).where((item)=>PairStrs.contains(item['symbol'])).toList();
 
   Symbol Function(String) str2symbol = (String sybl) {
-    switch(sybl) {
-      case 'BTCUSDT': return Symbol.BTC_USDT;
-      case 'ETHUSDT': return Symbol.ETH_USDT;
-      case 'XRPUSDT': return Symbol.XRP_USDT;
-      case 'BNBUSDT': return Symbol.BNB_USDT;
-      default: throw Exception("例外発生!! 未サポートペア =" + sybl);
-    }
-  };
+                            switch(sybl) {
+                              case 'BTCUSDT': return Symbol.BTC_USDT;
+                              case 'ETHUSDT': return Symbol.ETH_USDT;
+                              case 'XRPUSDT': return Symbol.XRP_USDT;
+                              case 'BNBUSDT': return Symbol.BNB_USDT;
+                              default: throw Exception("例外発生!! 未サポートペア =" + sybl);
+                            }
+                          };
 
   Map<Symbol, AskBidDataforTbl> ret = {};
   for(final item in retjson2) {
     if(item.containsKey('symbol') && item.containsKey('bidPrice') && item.containsKey('askPrice')) {
       Symbol sym = str2symbol(item['symbol']);
       ret[sym] = AskBidDataforTbl(sym, BrokerId.bi, item['askPrice'], item['bidPrice']);
+    }
+  }
+
+  return ret;
+}
+
+/* FTXからAsk/Bid取得 */
+Future<Map<Symbol, AskBidDataforTbl>> getTickerFx(Set<Symbol> pairs) async {
+  final Set<String> PairStrs =  pairs.map((s)=>s.str.replaceFirst("_", "/")).toSet();
+
+  final String endpoint = 'https://ftx.com/api';
+  final String method = '/markets';
+
+  final http.Response retRes = await http.get(Uri.parse(endpoint+method), headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "POST, GET, OPTIONS, PUT, DELETE, HEAD",
+  });
+  final retjson2 = await json.decode(retRes.body)['result'].where((item)=>PairStrs.contains(item['name'])).toList();
+
+  Symbol Function(String) str2symbol = (String sybl) {
+                            switch(sybl) {
+                              case 'BTC/USDT': return Symbol.BTC_USDT;
+                              case 'ETH/USDT': return Symbol.ETH_USDT;
+                              case 'XRP/USDT': return Symbol.XRP_USDT;
+                              case 'BNB/USDT': return Symbol.BNB_USDT;
+                              default: throw Exception("例外発生!! 未サポートペア =" + sybl);
+                            }
+                          };
+
+  Map<Symbol, AskBidDataforTbl> ret = {};
+  for(final item in retjson2) {
+    if(item.containsKey('name') && item.containsKey('bid') && item.containsKey('ask')) {
+      Symbol sym = str2symbol(item['name']);
+      ret[sym] = AskBidDataforTbl(sym, BrokerId.fx, item['ask'].toString(), item['bid'].toString());
     }
   }
 
